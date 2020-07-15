a294fcc0f99ab81a9ad3b993dd28a41c00975485
vyshnavirajeevan <66151655+vyshnavirajeevan@users.noreply.github.com>
2020-07-14 23:43:35 -0400

    changes

--- index.js
+++ index.js
@@ -11,25 +11,9 @@
      
      car1X = 0;
      car1Y = 100;
-     car1V = 4;
-     
-     car2X = width;
-     car2Y = 150;
-     car2V = 1;
-     
-     car3X = 0;
-     car3Y = 200;
-     car3V = 2;
-     
-     car4X = width;
-     car4Y = 300;
-     car4V = 3;
    }
    
    function draw() {
-     background(backgroundColor);
-     // Code for gold goal line
-     // Code to display Frog
      moveCars();
      drawCars();
    }
@@ -38,25 +22,11 @@
      // Move the car
      
        car1X+=car1V
-       car2X-= car2V
-       car3X+= car3V
-       car4X-= car4V
+
        // Reset if it moves off screen
        if(car1X==width)
          {
            car1X = 0;
-         }
-       if(car2X==0)
-         {
-           car2X = width;
-         }
-       if(car3X==width)
-         {
-           car3X = 0;
-         }
-       if(car4X==0)
-         {
-           car4X = width;
          }
    }
    
@@ -64,14 +34,7 @@
    function drawCars() {
      // Code for car 1
      fill(0, 80, 80);
-     ellipse(car1X, car1Y, 40, 30);
-     // Code for additional cars
-     fill(100, 50, 100);
-     ellipse(car2X, car2Y, 40, 30);
-     fill(20, 50, 20);
-     ellipse(car3X, car3Y, 40, 30);
-     fill(100, 100, 50);
-     ellipse(car4X, car4Y, 40, 30);
+     ellipse(car1X, car1Y, 100, 100);
    }
    
    
