524d2f871055acded2c8e629d760cbd21c7430b5
vyshnavirajeevan <66151655+vyshnavirajeevan@users.noreply.github.com>
2020-07-14 23:23:03 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
-let img;
-function preload() {
-  img = loadImage('pw.jpg');
+/* global createCanvas, background, loadImage, image, tint*/
+let bg, x, y, imageWidth, imageHeight;
+
+  
+  imageWidth = 200;
+  imageHeight = 150;
+
+  
+function setup(){
+  //constructor ish
+  // We only want to load the logo once.
+  bg = loadImage("pw.jpg");
+  x=50;
+  y=50;
+
 }
-function setup() {
-  image(img, 0, 0);
-}
+
+
+
+
+image(bg, x, y, imageWidth, imageHeight);
