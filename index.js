a61bd989b56c14b9c6ebcf5dcdc19f208bfd46d5
vyshnavirajeevan <66151655+vyshnavirajeevan@users.noreply.github.com>
2020-07-30 11:49:41 -0400

    Floating Circles

--- index.js
+++ index.js
@@ -1,40 +1,73 @@
+// Content behind double slashes is a comment. Use it for plain English notes,
+// or for code that you want to temporarily disable.
+/* global createCanvas, background, colorMode, HSL, windowWidth, windowHeight,
+   random, width, height, ellipse, text, fill, noStroke */
 
-/* global createCanvas, background, colorMode, HSB, random, width, height,
-   ellipse, mouseX, mouseY, text, fill, keyCode, textSize */
-
-   let backgroundColor, car1X, car1Y, car1V,car2V, car2X, car2Y, hit, car3X, car3Y, car3V, car4X, car4Y, car4V;
+   let dots, numberOfDots;
 
    function setup() {
-     // Canvas & color settings
-     createCanvas(500, 500);
-     colorMode(HSB, 360, 100, 100);
-     
-     car1X = 0;
-     car1Y = 100;
+     createCanvas(windowWidth - 40, windowHeight - 80);
+     colorMode(HSL, 360, 100, 100);
+     numberOfDots = 100;
+     dots = [];
+     for (let i = 0; i < numberOfDots; i++) {
+       dots.push(new BouncyDot());
+       //dots[i] = new BouncyDot();
+     }
    }
    
    function draw() {
-     moveCars();
-     drawCars();
+     background(220, 0, 95);
+     for (let i = 0; i < numberOfDots; i++) {
+       dots[i].float();
+       dots[i].display();
+     }
    }
    
-   function moveCars() {
-     // Move the car
-     
-       car1X+=car1V
-
-       // Reset if it moves off screen
-       if(car1X==width)
-         {
-           car1X = 0;
-         }
+   function mousePressed() {
+     // We'll use this for console log statements only.
+     console.log(dots[0].x);
    }
    
    
-   function drawCars() {
-     // Code for car 1
-     fill(0, 80, 80);
-     ellipse(car1X, car1Y, 100, 100);
+   class BouncyDot {
+     constructor() {
+       // Randomly generate position
+       this.x = random(width);
+       this.y = random(height);
+       // Randomly generate radius
+       this.r = random(5, 12);
+       // Randomly generate color
+       this.color = random(360);
+       // Randomly generate a master velocity (broken into components)...
+       this.masterXvelocity = random(0.5, 3);
+       this.masterYvelocity = random(0.5, 3);
+       // ...and use those as starting velocities.
+       this.xVelocity = this.masterXvelocity;
+       this.yVelocity = this.masterYvelocity;
+     }
+   
+     float() {
+       this.x += this.xVelocity;
+       this.y += this.yVelocity;
+       // Standard bounce code - like the DVD logo, but for spheres.
+       if (this.x + this.r > width) {
+         this.xVelocity = -1 * this.masterXvelocity;
+       }
+       if (this.x - this.r < 0) {
+         this.xVelocity = this.masterXvelocity;
+       }
+       if (this.y + this.r > height) {
+         this.yVelocity = -1 * this.masterYvelocity;
+       }
+       if (this.y - this.r < 0) {
+         this.yVelocity = this.masterYvelocity;
+       }
+     }
+   
+     display() {
+       fill(this.color, 80, 70);
+       noStroke();
+       ellipse(this.x, this.y, this.r * 2);
+     }
    }
-   
-   
